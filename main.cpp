b2346d3dae65daaea6cbc9cb62d7dfb4b52a41fb
hhayoung <76401721+hhayoung@users.noreply.github.com>
2021-01-12 18:01:49 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,29 +10,28 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-    //VideoCapture capture("./Traffic_Light.mp4");
-    VideoCapture capture("./test.mp4");
+    VideoCapture capture("./Traffic_Light.mp4");
 
     if (!capture.isOpened()) {
         printf("Can't open the video");
         return 0;
     }
 
-    //opencv ¿ÃπÃ¡ˆ ∫Øºˆ
-    Mat frame;
+    //opencv Ïù¥ÎØ∏ÏßÄ Î≥ÄÏàò
+    Mat frame_light;
 
     while (1) {
-        capture >> frame;
-        if (frame.empty()) break;
+        capture >> frame_light;
+        if (frame_light.empty()) break;
 
-        Mat gray;
-        cvtColor(frame, gray, CV_BGR2GRAY);
-        Mat blur;
-        GaussianBlur(gray, blur, Size(0, 0), 1.0);
+        Mat gray_light;
+        cvtColor(frame_light, gray_light, CV_BGR2GRAY);
+        Mat blur_light;
+        GaussianBlur(gray_light, blur_light, Size(0, 0), 1.0);
 
-        // ø¯ ∞À√‚
+        // Ïõê Í≤ÄÏ∂ú
         vector<Vec3f> circles;
-        HoughCircles(blur, circles, CV_HOUGH_GRADIENT, 1, 50, 120, 50, 50, 90);
+        HoughCircles(blur_light, circles, CV_HOUGH_GRADIENT, 1, 50, 120, 50, 50, 90);
 
         for (size_t i = 0; i < circles.size(); i++) {
             Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
@@ -42,41 +41,41 @@ int main(int argc, char** argv) {
             int x1 = int(cvRound(circles[i][0] - radius));
             int y1 = int(cvRound(circles[i][1] - radius));
             Rect rect(x1, y1, 2 * radius, 2 * radius);
-            Mat crop = frame(rect);
+            Mat crop_light = frame(rect);
             //imshow("crop", crop);
 
             int cw = rect.size().width;
             int ch = rect.size().height;
 
-            // Ω≈»£µÓ øµø™ ROI mask øµªÛ
-            Mat mask(cw, ch, CV_8UC1, Scalar::all(0));
+            // Ïã†Ìò∏Îì± ÏòÅÏó≠ ROI mask ÏòÅÏÉÅ
+            Mat mask_light(cw, ch, CV_8UC1, Scalar::all(0));
             Point crop_center(int(cw / 2), int(ch / 2));
-            circle(mask, crop_center, radius, Scalar::all(255) , -1, 8, 0);
+            circle(mask_light, crop_center, radius, Scalar::all(255) , -1, 8, 0);
 
             //imshow("mask", mask);
 
-            // ªˆ ¿ŒΩƒ
-            Mat hsv;
-            cvtColor(crop, hsv, CV_BGR2HSV);
+            // ÏÉâ Ïù∏Ïãù
+            Mat hsv_light;
+            cvtColor(crop_light, hsv_light, CV_BGR2HSV);
             vector<Mat> channels;
-            split(hsv, channels);
+            split(hsv_light, channels);
             channels[0] += 30;
-            merge(channels, hsv);
+            merge(channels, hsv_light);
 
-            float mean_hue = mean(hsv, mask)[0];
+            float mean_hue_light = mean(hsv_light, mask_light)[0];
 
-            printf("%f \n", mean_hue);
+            printf("%f \n", mean_hue_light);
             
             string color = "none";
-            if (mean_hue > 0 && mean_hue < 60) {
+            if (mean_hue_light > 0 && mean_hue_light < 60) {
                 color = "red";
             }
-            else if (mean_hue > 70 && mean_hue < 150) {
+            else if (mean_hue_light > 70 && mean_hue_light < 150) {
                 color = "green";
             }
-            putText(frame, color, center, CV_FONT_HERSHEY_SIMPLEX, 0.75, Scalar::all(255));
+            putText(frame_light, color, center, CV_FONT_HERSHEY_SIMPLEX, 0.75, Scalar::all(255));
 
-            imshow("frame", frame);
+            imshow("frame", frame_light);
         }
 
         if (waitKey(20) == 27) break;
